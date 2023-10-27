12c19e7d8b083ee99cf4580674faffee085c77ee
Eduardo Basso <eduardofigueiredobasso@gmail.com>
2023-10-27 17:38:16 -0300

    Concluído área administrativa

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,6 +10,16 @@ from django.utils import timezone
 
 # category (foreign key), show (boolean), owner (foreign key)
 # picture (imagem)
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
 
 # Cria tabelas 
 class Contact(models.Model):
@@ -20,6 +31,16 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/%d')
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        )
+    owner = models.ForeignKey(
+        User, 
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        )
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
