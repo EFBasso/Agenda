da36099c55717bad0c658d019cb01bb1a3badfa3
Eduardo Basso <eduardofigueiredobasso@gmail.com>
2023-10-27 12:09:40 -0300

    Configurando arquivos estáticos

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+# id (primary key - automático) 
+# first_name (string), lasta_name (string), phone (string)
+# email (email), created_date (date), description (text)
+
+# category (foreign key), show (boolean), owner (foreign key)
+# picture (imagem)
+
+# Cria tabelas 
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/%d')
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
