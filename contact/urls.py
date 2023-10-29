a13fa575a68c8a68448eb64043160c14b308dc57
Eduardo Basso <eduardofigueiredobasso@gmail.com>
2023-10-29 18:21:58 -0300

    adição de filtros

--- contact/urls.py
+++ contact/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from contact.views import contact_views
 from contact import views
 
 app_name = 'contact'
 
 urlpatterns = [
+    path('<int:contact_id>/', views.contact, name='contact'),
+    path('search/', views.search, name='search'),
     path('', views.index, name='index'),
 ]
+
