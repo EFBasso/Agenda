539d1b81abf35dd0a958d47b4bb0a6dce154410c
Eduardo Basso <eduardofigueiredobasso@gmail.com>
2023-10-30 07:26:49 -0300

    paginação

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -2,15 +2,22 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from contact.models import Contact
 from django.http import Http404
+from django.core.paginator import Paginator
 
 
 def index(request):
     contacts = Contact.objects \
         .filter(show=True) \
-        .order_by('-id')[10:20]
+        .order_by('-id')
+    
+    paginator = Paginator(contacts, 8)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    print('PAGE_OBJ', page_obj)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - ',
     }
 
@@ -38,10 +45,15 @@ def search(request):
             Q(email__icontains=search_value) 
             )\
         .order_by('-id')
+    
+    paginator = Paginator(contacts, 8)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Search - ',
+        'search_value': search_value,
     }
 
     return render(
