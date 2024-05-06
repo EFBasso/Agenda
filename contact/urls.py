d97edc308a7bf2de0aaf008edfce6a925572e021
Eduardo Figueiredo Basso <eduardofigueiredobasso@gmail.com>
2024-05-06 17:40:06 -0300

    Exemplos de organização de urls (CRUD)

--- contact/urls.py
+++ contact/urls.py
@@ -4,7 +4,23 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),
-    path('<search/', views.search, name='search'),
     path('', views.index, name='index'),
+    path('<search/', views.search, name='search'),
+    # contact (CRUD)
+    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
 ]
+
+"""
+PADRÃO URLS (exemplos que facilitam entender o que a url faz)
+    # contact (CRUD)
+    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+    path('contact/create/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/update/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/delete/', views.contact, name='contact'),
+
+    # users (CRUD)
+    path('user/<int:user_id>/detail/', views.user, name='user'),
+    path('user/create/', views.user, name='user'),
+    path('user/<int:user_id>/update/', views.user, name='user'),
+    path('user/<int:user_id>/delete/', views.user, name='user'),
+"""
