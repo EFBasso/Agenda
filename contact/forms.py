f78bf3f0843306aacdf0818a9f8e4c79c37daf2b
Eduardo Figueiredo Basso <eduardofigueiredobasso@gmail.com>
2024-05-09 16:07:30 -0300

    Link no menu para Criar contato

--- contact/forms.py
+++ contact/forms.py
@@ -14,56 +14,19 @@ class ContactForm(forms.ModelForm):
         help_text='Texto de ajuda',
     )
 
-    # Outro_campo = forms.CharField(
-    #      widget=forms.TextInput({
-    #         'class':'classe-a classe-b',
-    #         'placeholder': 'Outro campo'
-    #         }
-    #     ),
-    #     label='Outro campo',
-    #     help_text='Texto de ajuda',
-    # )
-
-    # def __init__(self, *args, **kwargs):
-    #         super().__init__(*args, **kwargs)
-
-            # self.fields['first_name'].widget.attrs.update({
-            #     'class':'classe-a classe-b',
-            #     'placeholder': 'O texto veio do __init__'
-            # })
-
-
-    class Meta: 
+    class Meta:
         model = models.Contact
+        fields = (
+            'first_name', 'last_name', 'phone',         
+            'email', 'description', 'category'
+        )       
 
-        fields = 'first_name', 'last_name', 'phone'
-    
-        # widgets = {
-        #     'first_name': forms.TextInput(
-        #         attrs={
-        #             'class':'classe-a classe-b',
-        #             'placeholder': 'Escreva aqui'
-        #         }
-        #     )
-        # }
-            
-    
-    
-
-    def clean(self):       
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid',
-            )
-        )
-
-        self.add_error(
-            'last_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid',
-            )
-        )
-        return super().clean()
+    # def clean(self):       
+    #     self.add_error(
+    #         'first_name',
+    #         ValidationError(
+    #             'Mensagem de erro',
+    #             code='invalid',
+    #         )
+    #     )
+    #     return super().clean()
