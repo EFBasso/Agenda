4aae842b02637c5526bd809d40da14c04511d0a9
Eduardo Figueiredo Basso <eduardofigueiredobasso@gmail.com>
2024-05-10 08:41:13 -0300

    Adicionado carregamento e exibição de imagens do contato

--- contact/forms.py
+++ contact/forms.py
@@ -4,29 +4,18 @@ from . import models
 from django.core.exceptions import ValidationError
 
 class ContactForm(forms.ModelForm):
-    first_name = forms.CharField(
-         widget=forms.TextInput({
-            'class':'classe-a classe-b',
-            'placeholder': 'Alterando o campo'
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'accept': 'imge/*'
             }
-        ),
-        label='Primeiro Nome',
-        help_text='Texto de ajuda',
+        )
     )
-
+    
     class Meta:
         model = models.Contact
         fields = (
             'first_name', 'last_name', 'phone',         
-            'email', 'description', 'category'
+            'email', 'description', 'category',
+            'picture'
         )       
-
-    # def clean(self):       
-    #     self.add_error(
-    #         'first_name',
-    #         ValidationError(
-    #             'Mensagem de erro',
-    #             code='invalid',
-    #         )
-    #     )
-    #     return super().clean()
