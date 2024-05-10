a516189a5c1f87dccd9251f4ea6a36a4315c3e3a
Eduardo Figueiredo Basso <eduardofigueiredobasso@gmail.com>
2024-05-09 23:38:02 -0300

    Redirecionando para atualizar contato de create para update

--- contact/urls.py
+++ contact/urls.py
@@ -1,3 +1,4 @@
+# flake8:noqa
 from django.urls import path
 from contact import views
 
@@ -6,22 +7,9 @@ app_name = 'contact'
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
+
     # contact (CRUD)
     path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
     path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
 ]
-
-"""
-PADRÃO URLS (exemplos que facilitam entender o que a url faz)
-    # contact (CRUD)
-    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
-    path('contact/create/', views.contact, name='contact'),
-    path('contact/<int:contact_id>/update/', views.contact, name='contact'),
-    path('contact/<int:contact_id>/delete/', views.contact, name='contact'),
-
-    # users (CRUD)
-    path('user/<int:user_id>/detail/', views.user, name='user'),
-    path('user/create/', views.user, name='user'),
-    path('user/<int:user_id>/update/', views.user, name='user'),
-    path('user/<int:user_id>/delete/', views.user, name='user'),
-"""
